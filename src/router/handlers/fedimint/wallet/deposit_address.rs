d36cf279ac480843f59dcc2a3c0c3099f1e183a3
kody.low <kodylow7@gmail.com>
2024-01-06 13:20:38 -0800

    fix: create address wrong instruction

--- src/router/handlers/fedimint/wallet/deposit_address.rs
+++ src/router/handlers/fedimint/wallet/deposit_address.rs
@@ -1,95 +1,41 @@
 use crate::{error::AppError, state::AppState};
-use anyhow::anyhow;
-use axum::{extract::State, http::StatusCode, Json};
+use axum::{extract::State, Json};
 use bitcoin::Address;
-use bitcoin_hashes::hex::ToHex;
-use fedimint_core::BitcoinAmountOrAll;
-use fedimint_wallet_client::{WalletClientModule, WithdrawState};
-use futures::StreamExt;
+use fedimint_core::{core::OperationId, time::now};
+use fedimint_wallet_client::WalletClientModule;
 use serde::{Deserialize, Serialize};
 use serde_json::{json, Value};
-use tracing::info;
+use std::time::Duration;
 
 #[derive(Debug, Deserialize)]
-pub struct WithdrawRequest {
-    pub address: Address,
-    pub amount_msat: BitcoinAmountOrAll,
+pub struct DepositAddressRequest {
+    pub timeout: u64,
 }
 
 #[derive(Debug, Serialize)]
-pub struct WithdrawResponse {
-    pub txid: String,
-    pub fees_sat: u64,
+pub struct DepositAddressResponse {
+    pub address: Address,
+    pub operation_id: OperationId,
 }
 
-async fn _withdraw(state: AppState, req: WithdrawRequest) -> Result<WithdrawResponse, AppError> {
+async fn _deposit_address(
+    state: AppState,
+    req: DepositAddressRequest,
+) -> Result<DepositAddressResponse, AppError> {
     let wallet_module = state.fm.get_first_module::<WalletClientModule>();
-    let (amount, fees) = match req.amount_msat {
-        // If the amount is "all", then we need to subtract the fees from
-        // the amount we are withdrawing
-        BitcoinAmountOrAll::All => {
-            let balance = bitcoin::Amount::from_sat(state.fm.get_balance().await.msats / 1000);
-            let fees = wallet_module
-                .get_withdraw_fees(req.address.clone(), balance)
-                .await?;
-            let amount = balance.checked_sub(fees.amount());
-            if amount.is_none() {
-                Err(AppError::new(
-                    StatusCode::BAD_REQUEST,
-                    anyhow!("Insufficient balance to pay fees"),
-                ))?;
-            }
-            (amount.unwrap(), fees)
-        }
-        BitcoinAmountOrAll::Amount(amount) => (
-            amount,
-            wallet_module
-                .get_withdraw_fees(req.address.clone(), amount)
-                .await?,
-        ),
-    };
-    let absolute_fees = fees.amount();
-
-    info!("Attempting withdraw with fees: {fees:?}");
-
-    let operation_id = wallet_module
-        .withdraw(req.address, amount, fees, ())
+    let (operation_id, address) = wallet_module
+        .get_deposit_address(now() + Duration::from_secs(req.timeout), ())
         .await?;
 
-    let mut updates = wallet_module
-        .subscribe_withdraw_updates(operation_id)
-        .await?
-        .into_stream();
-
-    while let Some(update) = updates.next().await {
-        info!("Update: {update:?}");
-
-        match update {
-            WithdrawState::Succeeded(txid) => {
-                return Ok(WithdrawResponse {
-                    txid: txid.to_hex(),
-                    fees_sat: absolute_fees.to_sat(),
-                });
-            }
-            WithdrawState::Failed(e) => {
-                return Err(AppError::new(
-                    StatusCode::INTERNAL_SERVER_ERROR,
-                    anyhow!("Withdraw failed: {:?}", e),
-                ));
-            }
-            _ => continue,
-        };
-    }
-
-    Err(AppError::new(
-        StatusCode::INTERNAL_SERVER_ERROR,
-        anyhow!("Update stream ended without outcome"),
-    ))
+    Ok(DepositAddressResponse {
+        address,
+        operation_id,
+    })
 }
 
 pub async fn handle_ws(v: Value, state: AppState) -> Result<Value, AppError> {
-    let v = serde_json::from_value(v).unwrap();
-    let withdraw = _withdraw(state, v).await?;
+    let v: DepositAddressRequest = serde_json::from_value(v).unwrap();
+    let withdraw = _deposit_address(state, v).await?;
     let withdraw_json = json!(withdraw);
     Ok(withdraw_json)
 }
@@ -97,8 +43,8 @@ pub async fn handle_ws(v: Value, state: AppState) -> Result<Value, AppError> {
 #[axum_macros::debug_handler]
 pub async fn handle_rest(
     State(state): State<AppState>,
-    Json(req): Json<WithdrawRequest>,
-) -> Result<Json<WithdrawResponse>, AppError> {
-    let withdraw = _withdraw(state, req).await?;
+    Json(req): Json<DepositAddressRequest>,
+) -> Result<Json<DepositAddressResponse>, AppError> {
+    let withdraw = _deposit_address(state, req).await?;
     Ok(Json(withdraw))
 }
