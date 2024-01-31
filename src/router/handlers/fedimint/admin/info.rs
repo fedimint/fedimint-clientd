655c91439bdc9e94432535503e9965ed062f07eb
kody.low <kodylow7@gmail.com>
2024-01-30 17:14:02 -0800

    fix: get info for all federations

--- src/router/handlers/fedimint/admin/info.rs
+++ src/router/handlers/fedimint/admin/info.rs
@@ -1,11 +1,11 @@
-use std::collections::BTreeMap;
+use std::collections::{BTreeMap, HashMap};
 
 use anyhow::Error;
 use axum::{extract::State, Json};
-use fedimint_client::ClientArc;
 use fedimint_core::{config::FederationId, Amount, TieredSummary};
 use fedimint_mint_client::MintClientModule;
 use fedimint_wallet_client::WalletClientModule;
+use multimint::MultiMint;
 use serde::Serialize;
 use serde_json::{json, Value};
 
@@ -22,38 +22,46 @@ pub struct InfoResponse {
     pub denominations_msat: TieredSummary,
 }
 
-async fn _info(client: ClientArc) -> Result<InfoResponse, Error> {
-    let mint_client = client.get_first_module::<MintClientModule>();
-    let wallet_client = client.get_first_module::<WalletClientModule>();
-    let summary = mint_client
-        .get_wallet_summary(
-            &mut client
-                .db()
-                .begin_transaction_nc()
-                .await
-                .to_ref_with_prefix_module_id(1),
-        )
-        .await;
-    Ok(InfoResponse {
-        federation_id: client.federation_id(),
-        network: wallet_client.get_network().to_string(),
-        meta: client.get_config().global.meta.clone(),
-        total_amount_msat: summary.total_amount(),
-        total_num_notes: summary.count_items(),
-        denominations_msat: summary,
-    })
+async fn _info(multimint: MultiMint) -> Result<HashMap<FederationId, InfoResponse>, Error> {
+    let mut info = HashMap::new();
+
+    for (id, client) in multimint.clients.lock().await.iter() {
+        let mint_client = client.get_first_module::<MintClientModule>();
+        let wallet_client = client.get_first_module::<WalletClientModule>();
+        let summary = mint_client
+            .get_wallet_summary(
+                &mut client
+                    .db()
+                    .begin_transaction_nc()
+                    .await
+                    .to_ref_with_prefix_module_id(1),
+            )
+            .await;
+
+        info.insert(
+            *id,
+            InfoResponse {
+                federation_id: client.federation_id(),
+                network: wallet_client.get_network().to_string(),
+                meta: client.get_config().global.meta.clone(),
+                total_amount_msat: summary.total_amount(),
+                total_num_notes: summary.count_items(),
+                denominations_msat: summary,
+            },
+        );
+    }
+
+    Ok(info)
 }
 
 pub async fn handle_ws(state: AppState, _v: Value) -> Result<Value, AppError> {
-    let client = state.get_client(None).await?;
-    let info = _info(client).await?;
+    let info = _info(state.multimint).await?;
     let info_json = json!(info);
     Ok(info_json)
 }
 
 #[axum_macros::debug_handler]
-pub async fn handle_rest(State(state): State<AppState>) -> Result<Json<InfoResponse>, AppError> {
-    let client = state.get_client(None).await?;
-    let info = _info(client).await?;
+pub async fn handle_rest(State(state): State<AppState>) -> Result<Json<HashMap<FederationId, InfoResponse>>, AppError> {
+    let info = _info(state.multimint).await?;
     Ok(Json(info))
 }
