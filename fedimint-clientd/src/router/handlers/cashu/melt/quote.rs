c23cb1cefaa8c82befa3e90649e354b2c333367e
richarddushime <mudaherarich@gmail.com>
2024-03-21 19:07:22 +0300

    Cashu v1 Endpoints for Melt and mint quote

--- fedimint-clientd/src/router/handlers/cashu/melt/quote.rs
+++ fedimint-clientd/src/router/handlers/cashu/melt/quote.rs
@@ -1,16 +1,63 @@
-use axum::extract::State;
+use std::u64;
 
+use axum::extract::State;
+use axum::http::StatusCode;
+use axum::Json;
+use serde::Deserialize;
+use serde::Serialize;
+use anyhow::anyhow;
+use std::string::String;
 use crate::error::AppError;
 use crate::state::AppState;
+
+#[derive(Debug, Deserialize)]
+enum MeltQuoteUnit {
+    Sat,
+}
+#[derive(Debug, Deserialize)]
+pub struct PostMeltQuoteRequest {
+    request: String,
+    unit: MeltQuoteUnit,
+}
+#[derive(Debug, Serialize)]
+pub struct PostMeltQuoteResponse {
+    quote: String,
+    amount: u64,
+    fee_reserve: u64,
+    paid: bool,
+    expiry: u64,
+}
 
 #[axum_macros::debug_handler]
 pub async fn handle_method(State(_state): State<AppState>) -> Result<(), AppError> {
     // TODO: Implement this function
+
     Ok(())
 }
 
 #[axum_macros::debug_handler]
-pub async fn handle_method_quote_id(State(_state): State<AppState>) -> Result<(), AppError> {
+pub async fn handle_method_quote_id(
+    State(state): State<AppState>,
+    Json(req): Json<PostMeltQuoteRequest>,
+) -> Result<Json<PostMeltQuoteResponse>, AppError> {
     // TODO: Implement this function
-    Ok(())
+    let client = match state.multimint.get_default().await {
+        Some(client) => Ok(client),
+            None => Err(AppError::new(
+                StatusCode::BAD_REQUEST,
+                anyhow!("No default client found "),)),
+    };
+
+    let amount=client.as_ref().unwrap().get_balance().await;
+
+    let response= PostMeltQuoteResponse{
+        quote: "me".to_string(),
+        amount: amount.msats,
+        fee_reserve: 2,
+        paid: true,
+        expiry: 1701704757,
+
+    };
+
+    Ok(Json(response))
 }
--- fedimint-clientd/src/router/handlers/cashu/mint/quote.rs
+++ fedimint-clientd/src/router/handlers/cashu/mint/quote.rs
@@ -1,7 +1,32 @@
 use axum::extract::State;
+use axum::http::StatusCode;
+use axum::Json;
+use serde::{Deserialize, Serialize};
+use anyhow::anyhow;
 
 use crate::error::AppError;
 use crate::state::AppState;
+
+#[derive(Debug, Deserialize)]
+enum MintQuoteUnit {
+    Sat,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct PostMintQuoteRequest {
+    amount: u64,
+    unit: MintQuoteUnit,
+
+}
+
+#[derive(Debug, Serialize)]
+pub struct PostMintQuoteResponse {
+    quote: String,
+    request: String,
+    paid: bool,
+    expiry: u64,
+}
+
 
 #[axum_macros::debug_handler]
 pub async fn handle_method(State(_state): State<AppState>) -> Result<(), AppError> {
@@ -10,7 +35,22 @@ pub async fn handle_method(State(_state): State<AppState>) -> Result<(), AppErro
 }
 
 #[axum_macros::debug_handler]
-pub async fn handle_method_quote_id(State(_state): State<AppState>) -> Result<(), AppError> {
+pub async fn handle_method_quote_id(State(state): State<AppState>,
+Json(req): Json<PostMintQuoteRequest>,
+) -> Result<Json<PostMintQuoteResponse>, AppError> {
     // TODO: Implement this function
-    Ok(())
+
+    let client = match state.multimint.get_default().await {
+        Some(client) => Ok(client),
+            None => Err(AppError::new(StatusCode::BAD_REQUEST, anyhow!("No default client"),)),
+    };
+
+    let response = PostMintQuoteResponse{
+        quote: "hi".to_string(),
+        request: "jhfdjm".to_string(),
+        paid: false,
+        expiry: 1012330,
+    };
+
+    Ok(Json(response))
 }
