90a8c36604a31874fb6cb46233c4b459742333fb
kody.low <kodylow7@gmail.com>
2024-03-29 09:04:27 -0700

    chore: bump to fedimint rc.3 and update example.env

--- flake.nix
+++ flake.nix
@@ -1,10 +1,9 @@
 {
-  description = "A fedimint client daemon for server side applications to hold, use, and manage Bitcoin";
+  description =
+    "A fedimint client daemon for server side applications to hold, use, and manage Bitcoin";
 
   inputs = {
-    nixpkgs = {
-      url = "github:nixos/nixpkgs/nixos-23.11";
-    };
+    nixpkgs = { url = "github:nixos/nixpkgs/nixos-23.11"; };
 
     flakebox = {
       url = "github:rustshop/flakebox";
@@ -13,7 +12,7 @@
 
     fedimint = {
       url =
-        "github:fedimint/fedimint?rev=9d552fdf82f4af429165a1fd409615809ada4058";
+        "github:fedimint/fedimint?rev=c32bbe8ed8d95ae420b5879e92dda6ce48f5c914";
     };
 
     flake-utils.url = "github:numtide/flake-utils";
@@ -23,8 +22,8 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         nixpkgs = fedimint.inputs.nixpkgs;
-        pkgs = import nixpkgs { 
-          inherit system; 
+        pkgs = import nixpkgs {
+          inherit system;
           overlays = fedimint.overlays.fedimint;
         };
         lib = pkgs.lib;
@@ -47,51 +46,39 @@
         };
 
         # all standard toolchains provided by flakebox
-        toolchainsStd =
-          flakeboxLib.mkStdFenixToolchains toolchainArgs;
+        toolchainsStd = flakeboxLib.mkStdFenixToolchains toolchainArgs;
 
-        toolchainsNative = (pkgs.lib.getAttrs
-          [
-            "default"
-          ]
-          toolchainsStd
-        );
+        toolchainsNative = (pkgs.lib.getAttrs [ "default" ] toolchainsStd);
 
-        toolchainNative = flakeboxLib.mkFenixMultiToolchain {
-          toolchains = toolchainsNative;
-        };
+        toolchainNative =
+          flakeboxLib.mkFenixMultiToolchain { toolchains = toolchainsNative; };
 
         commonArgs = {
-          buildInputs = [
-            pkgs.openssl
-          ] ++ lib.optionals pkgs.stdenv.isDarwin [
-            pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
-          ];
-          nativeBuildInputs = [
-            pkgs.pkg-config
-          ];
+          buildInputs = [ pkgs.openssl ] ++ lib.optionals pkgs.stdenv.isDarwin
+            [ pkgs.darwin.apple_sdk.frameworks.SystemConfiguration ];
+          nativeBuildInputs = [ pkgs.pkg-config ];
         };
-        outputs = (flakeboxLib.craneMultiBuild { toolchains = toolchainsStd; }) (craneLib':
-          let
-            craneLib = (craneLib'.overrideArgs {
-              pname = "flexbox-multibuild";
-              src = rustSrc;
-            }).overrideArgs commonArgs;
-          in
-          rec {
-            workspaceDeps = craneLib.buildWorkspaceDepsOnly { };
-            workspaceBuild =
-              craneLib.buildWorkspace { cargoArtifacts = workspaceDeps; };
-            fedimint-clientd = craneLib.buildPackageGroup
-              { pname = "fedimint-clientd"; packages = [ "fedimint-clientd" ]; mainProgram = "fedimint-clientd"; };
-          });
-      in
-      {
+        outputs = (flakeboxLib.craneMultiBuild { toolchains = toolchainsStd; })
+          (craneLib':
+            let
+              craneLib = (craneLib'.overrideArgs {
+                pname = "flexbox-multibuild";
+                src = rustSrc;
+              }).overrideArgs commonArgs;
+            in rec {
+              workspaceDeps = craneLib.buildWorkspaceDepsOnly { };
+              workspaceBuild =
+                craneLib.buildWorkspace { cargoArtifacts = workspaceDeps; };
+              fedimint-clientd = craneLib.buildPackageGroup {
+                pname = "fedimint-clientd";
+                packages = [ "fedimint-clientd" ];
+                mainProgram = "fedimint-clientd";
+              };
+            });
+      in {
         legacyPackages = outputs;
-        packages = {
-          default = outputs.fedimint-clientd;
-        };
-         devShells = fmLib.devShells // {
+        packages = { default = outputs.fedimint-clientd; };
+        devShells = fmLib.devShells // {
           default = fmLib.devShells.default.overrideAttrs (prev: {
             nativeBuildInputs = [
               pkgs.mprocs
