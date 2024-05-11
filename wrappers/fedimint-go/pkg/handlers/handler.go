bed4489f2d2a44f4f8d07e179b8ffdccf99456e3
yemmyharry <yemmyharry@gmail.com>
2024-05-11 11:34:26 +0100

    feat: add create invoice, invoice and  config methods

--- wrappers/fedimint-go/pkg/handlers/handler.go
+++ wrappers/fedimint-go/pkg/handlers/handler.go
@@ -1,12 +1,17 @@
 package handlers
 
 import (
+	"fedimint-go-client/pkg/fedimint"
+	"fmt"
 	"html/template"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 type Handler struct {
 	Tmpl *template.Template
+	Fc   *fedimint.FedimintClient
 }
 
 func (h *Handler) Index(w http.ResponseWriter, r *http.Request) {
@@ -21,6 +26,85 @@ func (h *Handler) Index(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		http.Error(w, "Error executing template", http.StatusInternalServerError)
 		return
+	}
+
+}
+
+func (h *Handler) AdminConfigHandler(w http.ResponseWriter, r *http.Request) {
+	configData, err := h.Fc.Config()
+	if err != nil {
+		http.Error(w, "Error fetching config data: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = h.Tmpl.ExecuteTemplate(w, "admin_config.gohtml", map[string]interface{}{
+		"ConfigData": *configData,
+	})
+	if err != nil {
+		http.Error(w, "Error executing template: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func (h *Handler) InvoiceHandler(w http.ResponseWriter, r *http.Request) {
+
+	err := h.Tmpl.ExecuteTemplate(w, "invoice.gohtml", nil)
+	if err != nil {
+		http.Error(w, "Error executing template: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func (h *Handler) CreateInvoiceHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+
+		amountMsatStr := r.FormValue("amountMsat")
+		if amountMsatStr == "" {
+			http.Error(w, "Amount (msat) is required", http.StatusBadRequest)
+			return
+		}
+		amountMsat, err := strconv.ParseUint(amountMsatStr, 10, 64)
+		if err != nil {
+			http.Error(w, "Invalid amountMsat: "+err.Error(), http.StatusBadRequest)
+			return
+		}
+		description := r.FormValue("description")
+		if description == "" {
+			http.Error(w, "Description is required", http.StatusBadRequest)
+			return
+		}
+		expTimeStr := r.FormValue("expiryTime")
+		expTime, err := strconv.Atoi(expTimeStr)
+		if err != nil {
+			http.Error(w, "Invalid expiryTime: "+err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		gwIDStr := r.FormValue("gatewayId")
+		fedIDStr := r.FormValue("federationId")
+
+		invoiceResponse, err := h.Fc.Ln.CreateInvoice(amountMsat, description, &expTime, &gwIDStr, &fedIDStr)
+		if err != nil {
+			// Check if the error message contains "malformed public key" indicating a problem with gatewayId
+			if strings.Contains(err.Error(), "malformed public key") {
+				http.Error(w, "Invalid gatewayId provided", http.StatusBadRequest)
+				return
+			}
+			http.Error(w, "Error creating invoice: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		err = h.Tmpl.ExecuteTemplate(w, "create-invoice.gohtml", invoiceResponse.Invoice)
+		if err != nil {
+			http.Error(w, "Error executing template: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+	} else {
+		err := h.Tmpl.ExecuteTemplate(w, "create-invoice.gohtml", nil)
+		if err != nil {
+			http.Error(w, "Error executing template: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
 	}
 
 }
