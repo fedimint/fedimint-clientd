ebef8a9d11a1e927d456005cb43dd50ebf1a9956
kody.low <kodylow7@gmail.com>
2024-05-25 12:31:02 -0700

    fix: better shutdown handling

--- fedimint-nwc/src/main.rs
+++ fedimint-nwc/src/main.rs
@@ -1,10 +1,9 @@
-use std::str::FromStr;
-
 use anyhow::Result;
 use clap::Parser;
 use config::Cli;
-use multimint::fedimint_core::api::InviteCode;
-use tracing::info;
+use tokio::signal::unix::SignalKind;
+use tokio::sync::oneshot;
+use tracing::{debug, info};
 
 pub mod config;
 pub mod nwc;
@@ -17,37 +16,52 @@ async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
     dotenv::dotenv().ok();
 
-    let cli: Cli = Cli::parse();
+    let cli = Cli::parse();
+    let db_path = cli.db_path.clone();
+    let keys_file = cli.keys_file.clone();
+    let mut state = AppState::new(db_path, &keys_file).await?;
 
-    let mut state = AppState::new(cli.db_path, &cli.keys_file).await?;
-
-    let manual_secret = match cli.manual_secret {
-        Some(secret) => Some(secret),
-        None => match std::env::var("FEDIMINT_CLIENTD_MANUAL_SECRET") {
-            Ok(secret) => Some(secret),
-            Err(_) => None,
-        },
-    };
-
-    match InviteCode::from_str(&cli.invite_code) {
-        Ok(invite_code) => {
-            let federation_id = state
-                .multimint
-                .register_new(invite_code, manual_secret)
-                .await?;
-            info!("Created client for federation id: {:?}", federation_id);
-        }
-        Err(e) => {
-            info!(
-                "No federation invite code provided, skipping client creation: {}",
-                e
-            );
-        }
-    }
+    let manual_secret = AppState::load_manual_secret(&cli).await;
+    let invite_code = cli.invite_code.clone();
+    state.init_multimint(&invite_code, manual_secret).await?;
 
     if state.multimint.all().await.is_empty() {
-        return Err(anyhow::anyhow!("No clients found, must have at least one client to start the server. Try providing a federation invite code with the `--invite-code` flag or setting the `FEDIMINT_CLIENTD_INVITE_CODE` environment variable."));
+        return Err(anyhow::anyhow!(
+            "No multimint clients found, must have at least one client to start the server."
+        ));
     }
 
+    // Shutdown signal handler
+    let (tx, rx) = oneshot::channel::<()>();
+    let signal_handler = tokio::spawn(handle_signals(tx));
+
+    // Start the event loop
+
+    // Wait for shutdown signal
+    info!("Server is running. Press CTRL+C to exit.");
+    let _ = rx.await;
+    info!("Shutting down...");
+    state.wait_for_active_requests().await;
+    let _ = signal_handler.await;
+
+    Ok(())
+}
+
+async fn handle_signals(tx: oneshot::Sender<()>) -> Result<()> {
+    let signals = tokio::signal::unix::signal(SignalKind::terminate())
+        .or_else(|_| tokio::signal::unix::signal(SignalKind::interrupt()));
+
+    match signals {
+        Ok(mut stream) => {
+            while stream.recv().await.is_some() {
+                debug!("Received shutdown signal");
+            }
+        }
+        Err(e) => {
+            return Err(anyhow::anyhow!("Failed to install signal handlers: {}", e));
+        }
+    }
+
+    let _ = tx.send(());
     Ok(())
 }
--- fedimint-nwc/src/state.rs
+++ fedimint-nwc/src/state.rs
@@ -1,18 +1,28 @@
+use std::collections::BTreeSet;
 use std::fs::{create_dir_all, File};
 use std::io::{BufReader, Write};
 use std::path::{Path, PathBuf};
+use std::str::FromStr;
+use std::sync::Arc;
+use std::time::Duration;
 
 use anyhow::{Context, Result};
+use multimint::fedimint_core::api::InviteCode;
 use multimint::MultiMint;
 use nostr_sdk::secp256k1::SecretKey;
-use nostr_sdk::Keys;
+use nostr_sdk::{EventId, Keys};
 use serde::{Deserialize, Serialize};
+use tokio::sync::Mutex;
+use tracing::{debug, error, info};
+
+use crate::config::Cli;
 
 #[derive(Debug, Clone)]
 pub struct AppState {
     pub multimint: MultiMint,
     pub user_keys: Keys,
     pub server_keys: Keys,
+    pub active_requests: Arc<Mutex<BTreeSet<EventId>>>,
 }
 
 impl AppState {
@@ -22,11 +32,52 @@ impl AppState {
 
         let keys = Nip47Keys::load_or_generate(keys_file)?;
 
+        let active_requests = Arc::new(Mutex::new(BTreeSet::new()));
+
         Ok(Self {
             multimint: clients,
             user_keys: keys.user_keys(),
             server_keys: keys.server_keys(),
+            active_requests,
         })
+    }
+
+    pub async fn load_manual_secret(cli: &Cli) -> Option<String> {
+        cli.manual_secret
+            .clone()
+            .or_else(|| std::env::var("FEDIMINT_CLIENTD_MANUAL_SECRET").ok())
+    }
+
+    pub async fn init_multimint(
+        &mut self,
+        invite_code: &str,
+        manual_secret: Option<String>,
+    ) -> Result<()> {
+        match InviteCode::from_str(invite_code) {
+            Ok(invite_code) => {
+                let federation_id = self
+                    .multimint
+                    .register_new(invite_code, manual_secret)
+                    .await?;
+                info!("Created client for federation id: {:?}", federation_id);
+                Ok(())
+            }
+            Err(e) => {
+                error!("Invalid federation invite code: {}", e);
+                Err(e.into())
+            }
+        }
+    }
+
+    pub async fn wait_for_active_requests(&self) {
+        let requests = self.active_requests.lock().await;
+        loop {
+            if requests.is_empty() {
+                break;
+            }
+            debug!("Waiting for {} requests to complete...", requests.len());
+            tokio::time::sleep(Duration::from_secs(1)).await;
+        }
     }
 }
 
