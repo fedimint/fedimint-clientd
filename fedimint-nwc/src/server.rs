88ced94042d7997ed7487c3e88fe2d55b4abcde2
Alex Lewin <alexlwn123@gmail.com>
2024-05-27 20:59:35 -0400

    feat: frontend progress

--- fedimint-nwc/src/server.rs
+++ fedimint-nwc/src/server.rs
@@ -4,7 +4,7 @@ use tower_http::services::ServeDir;
 use tracing::error;
 
 pub async fn run_server() -> Result<(), anyhow::Error> {
-    let server = Router::new().nest_service("/", ServeDir::new("frontend/assets"));
+    let server = Router::new().nest_service("/", ServeDir::new("frontend"));
     let listener = TcpListener::bind("0.0.0.0:3000").await?;
     axum::serve(listener, server).await.map_err(|e| {
         error!("Server failed to run: {}", e);
