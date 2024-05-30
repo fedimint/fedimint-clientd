8baef35db85aae7e6cf8d6366fc6d684561246f3
ndungudedan <dnkibere@gmail.com>
2024-05-30 21:22:27 +0300

    fix: fedimint-kotlin log inputs

--- wrappers/fedimint-kotlin/src/main/kotlin/com/fedimintClientd/Application.kt
+++ wrappers/fedimint-kotlin/src/main/kotlin/com/fedimintClientd/Application.kt
@@ -23,101 +23,101 @@ fun main() {
 
             logMethod("/v2/admin/info")
             val info = fedimintClient.info()
-            logInputAndOutput({}, info)
+            logInputAndOutput(emptyMap<String, Any>(), info)
 
             logMethod("/v2/admin/config")
             val config = fedimintClient.config()
-            logInputAndOutput({}, config)
+            logInputAndOutput(emptyMap<String, Any>(), config)
 
             logMethod("/v2/admin/discover-version")
             val version = fedimintClient.discoverVersion(1)
-            logInputAndOutput({}, version)
+            logInputAndOutput(emptyMap<String, Any>(), version)
 
             logMethod("/v2/admin/federation-ids")
             val federationId = fedimintClient.federationIds()
-            logInputAndOutput({}, federationId)
+            logInputAndOutput(emptyMap<String, Any>(), federationId)
 
             logMethod("/v2/admin/join")
             val inviteCode = dotenv["FEDIMINT_CLIENTD_INVITE_CODE"]
                 ?: "fed11qgqrgvnhwden5te0v9k8q6rp9ekh2arfdeukuet595cr2ttpd3jhq6rzve6zuer9wchxvetyd938gcewvdhk6tcqqysptkuvknc7erjgf4em3zfh90kffqf9srujn6q53d6r056e4apze5cw27h75"
             val join =
                 fedimintClient.join(inviteCode)
-            logInputAndOutput({ "inviteCode" to inviteCode }, join)
+            logInputAndOutput(mapOf( "inviteCode" to inviteCode ), join)
 
             logMethod("/v2/admin/list-operations")
             val operations = fedimintClient.listOperations(10)
-            logInputAndOutput({ "limit" to 10 }, operations)
+            logInputAndOutput(mapOf( "limit" to 10 ), operations)
 
             //        Onchain
 
             logMethod("/v2/onchain/deposit-address")
             val address = onchain.createDepositAddress(1000)
-            logInputAndOutput({ "timeout" to 1000 }, address)
+            logInputAndOutput(mapOf( "timeout" to 1000 ), address)
 
             logMethod("/v2/onchain/withdraw")
             val withdraw = address?.let { onchain.withdraw(it.address, 1000) }
-            logInputAndOutput({
-                "address" to address?.address
+            logInputAndOutput(mapOf(
+                "address" to address?.address,
                 "amountSat" to 1000
-            }, withdraw)
+            ), withdraw)
 
             //        Lightning
 
             logMethod("/v2/ln/list-gateways")
             val gateways = ln.listGateways()
-            logInputAndOutput({}, gateways)
+            logInputAndOutput(emptyMap<String, Any>(), gateways)
             if (gateways.isNotEmpty()) {
                 fedimintClient.activeGatewayId = gateways.first().info.gatewayId
             }
 
             logMethod("/v2/ln/invoice")
             val invoice = ln.createInvoice(1000, "Test")
-            logInputAndOutput({
-                "amountMsat" to 1000
+            logInputAndOutput(mapOf(
+                "amountMsat" to 1000,
                 "description" to "Test"
-            }, invoice)
+            ), invoice)
 
             logMethod("/v2/ln/await-invoice")
             val awaitInvoice = invoice?.let { ln.awaitInvoice(operationId = it.operationId) }
-            logInputAndOutput({ "operationId" to invoice?.operationId }, awaitInvoice)
+            logInputAndOutput(mapOf( "operationId" to invoice?.operationId ), awaitInvoice)
 
             logMethod("/v2/ln/pay")
             val pay = invoice?.let { ln.pay(paymentInfo = it.invoice) }
-            logInputAndOutput({ "paymentInfo" to invoice?.invoice }, pay)
+            logInputAndOutput(mapOf( "paymentInfo" to invoice?.invoice ), pay)
 
             val awaitPay = pay?.let { ln.awaitPay(operationId = it.operationId) }
-            logInputAndOutput({ "operationId" to pay?.operationId }, awaitPay)
+            logInputAndOutput(mapOf( "operationId" to pay?.operationId ), awaitPay)
 
             //        Mint
 
             logMethod("/v2/mint/spend")
             val spend = mint.spend(amountMsat = 3000, allowOverpay = true, timeout = 1000, includeInvite = false)
-            logInputAndOutput({
-                "amountMsat" to 3000
-                "allowOverpay" to true
-                "timeout" to 1000
+            logInputAndOutput(mapOf(
+                "amountMsat" to 3000,
+                "allowOverpay" to true,
+                "timeout" to 1000,
                 "includeInvite" to false
-            }, spend)
+            ), spend)
 
             logMethod("/v2/mint/decode-notes")
             val notes = spend?.notes?.let { mint.decodeNotes(it) }
-            logInputAndOutput({ "notes" to spend?.notes }, notes)
+            logInputAndOutput(mapOf( "notes" to spend?.notes ), notes)
 
             logMethod("/v2/mint/encode-notes")
             val encodedNotes = notes?.notesJson?.let { mint.encodeNotes(it) }
-            logInputAndOutput({ "notesJson" to notes?.notesJson }, encodedNotes)
+            logInputAndOutput(mapOf( "notesJson" to notes?.notesJson ), encodedNotes)
 
             logMethod("/v2/mint/validate")
             val validate = spend?.notes?.let { mint.validate(it) }
-            logInputAndOutput({ "notes" to spend?.notes }, validate)
+            logInputAndOutput(mapOf( "notes" to spend?.notes ), validate)
 
             logMethod("/v2/mint/reissue")
             val reissue = spend?.notes?.let { mint.reissue(it) }
-            logInputAndOutput({ "notes" to spend?.notes }, reissue)
+            logInputAndOutput(mapOf( "notes" to spend?.notes ), reissue)
 
             logMethod("/v2/mint/split")
             val split = spend?.notes?.let { mint.split(it) }
-            logInputAndOutput({ "notes" to spend?.notes }, split)
+            logInputAndOutput(mapOf( "notes" to spend?.notes ), split)
 
             println("🚀Done: All Methods Tested Successfully🚀")
         }
