92751f291fcbd82fed07b3cbf32bd1c442addd16
Conner Ow <conner@connerow.dev>
2024-07-05 09:52:45 -0500

    fix: add the Clone trait to InfoResponse (multimint)

--- multimint/src/types.rs
+++ multimint/src/types.rs
@@ -5,7 +5,7 @@ use fedimint_core::{Amount, TieredSummary};
 use serde::Serialize;
 
 /// InfoResponse for getting the Federation Config info
-#[derive(Debug, Serialize)]
+#[derive(Debug, Serialize, Clone)]
 #[serde(rename_all = "snake_case")]
 pub struct InfoResponse {
     pub federation_id: FederationId,
