290ac39002a98b6c69ea6928df5299aa17d64ecb
Conner Ow <conner@connerow.dev>
2024-07-06 16:06:04 -0500

    fix: add partialeq

--- multimint/src/types.rs
+++ multimint/src/types.rs
@@ -5,7 +5,7 @@ use fedimint_core::{Amount, TieredSummary};
 use serde::Serialize;
 
 /// InfoResponse for getting the Federation Config info
-#[derive(Debug, Serialize, Clone)]
+#[derive(Debug, Serialize, Clone, PartialEq)]
 #[serde(rename_all = "snake_case")]
 pub struct InfoResponse {
     pub federation_id: FederationId,
