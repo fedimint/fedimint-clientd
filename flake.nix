8886a25bd7a85a6dafd22533ce0692bac97acedf
Dawid Ciężarkiewicz <dpc@dpc.pw>
2024-10-07 14:29:14 -0700

    fix: stdenv on darwin

--- flake.nix
+++ flake.nix
@@ -73,14 +73,6 @@
                 pkgs.wasm-pack
               ] ++ lib.optionals (!pkgs.stdenv.isDarwin) [ pkgs.firefox ];
             };
-          }
-          // lib.optionalAttrs pkgs.stdenv.isDarwin {
-            # on Darwin newest stdenv doesn't seem to work
-            # linking rocksdb
-            stdenv = pkgs.clang11Stdenv;
-            clang = llvmPackages.clang;
-            libclang = llvmPackages.libclang.lib;
-            clang-unwrapped = llvmPackages.clang-unwrapped;
           };
 
         # all standard toolchains provided by flakebox
